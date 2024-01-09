06715272759cea0677a47e2104137d988a050dab
Irawadee <irawadee.pm@gmail.com>
2024-01-10 12:35:43 +1300

    FrontEnd/FirstFooter

---
+++ StaticProject/src/Components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Footer() {
+    const year = new Date().getFullYear();
+    return (
+        // style the footer
+        <footer style={{
+            backgroundColor: '#08123C',
+            padding: '20px',
+            color: '#FFFFFF',
+            textAlign: 'center'
+            // marginTop: 'auto', // Push the footer to the bottom
+        }}>
+            {`Copyright © Upbeat Code ${year}`}
+        </footer>
+    );
+}
+
+
+
+
